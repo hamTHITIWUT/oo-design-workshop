2b051611d6428f538a9a9e66702e3b6299e427cd
thitiwut.kra <thitiwut.kra@turbo.co.th>
2021-08-27 16:49:14 +0700

    [Range]: add getStart/End input

--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,11 +1,29 @@
 public class Range {
     private String input;
+    private static final int BEGIN_INDEX_START_NUMBER = 1;
+    private static final int END_INDEX_START_NUMBER = 2;
+    private static final int BEGIN_INDEX_END_NUMBER = 3;
+    private static final int END_START_END_NUMBER = 4;
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
 
     public Range(String input) {
         this.input = input;
     }
 
-    public boolean isStartWithBracket() {
-        return input.startsWith("[");
+    public boolean isStartWithInclusive() {
+        return input.startsWith(OPEN_BRACKET);
+    }
+
+    public int getStartInput() {
+        return Integer.parseInt(input.substring(BEGIN_INDEX_START_NUMBER, END_INDEX_START_NUMBER));
+    }
+
+    public int getEndInput() {
+        return Integer.parseInt(input.substring(BEGIN_INDEX_END_NUMBER, END_START_END_NUMBER));
+    }
+
+    public boolean isEndWithInclusive() {
+        return input.endsWith(CLOSE_BRACKET);
     }
 }
--- src/test/java/RangeTest.java
+++ src/test/java/RangeTest.java
@@ -1,14 +1,56 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RangeTest {
-    @DisplayName("is start with bracket should be true")
+    @DisplayName("is start with inclusive")
     @Test
-    public void isStartWithBracket() {
+    public void isStartWithInclusive() {
         Range range = new Range("[1,5]");
-        boolean actual = range.isStartWithBracket();
+        boolean actual = range.isStartWithInclusive();
         assertTrue(actual);
+    }
+
+    @DisplayName("is not start with bracket should be false.")
+    @Test
+    public void isNotStartWithBracket() {
+        Range range = new Range("(1,5]");
+        boolean actual = range.isStartWithInclusive();
+        assertFalse(actual);
+    }
+
+    @DisplayName("get start input [1,5] should be 1")
+    @Test
+    public void getStartInputShouldBe1() {
+        Range range = new Range("[1,5]");
+        int actual = range.getStartInput();
+        assertEquals(1, actual);
+
+    }
+
+    @DisplayName("get start input [1,5] should be 5")
+    @Test
+    public void getEndInputShouldBe5() {
+        Range range = new Range("[1,5]");
+        int actual = range.getEndInput();
+        assertEquals(5, actual);
+
+    }
+
+    @DisplayName("is end inclusive")
+    @Test
+    public void isEndWithInclusive() {
+        Range range = new Range("[1,5]");
+        boolean actual = range.isEndWithInclusive();
+        assertTrue(actual);
+    }
+
+    @DisplayName("is not end inclusive")
+    @Test
+    public void isNotEndWithInclusive() {
+        Range range = new Range("[1,5)");
+        boolean actual = range.isEndWithInclusive();
+        assertFalse(actual);
     }
 }
